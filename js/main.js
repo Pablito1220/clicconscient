f472e800b4e4e232e2827f048c91ee26693907d0
Pablito1220 <31880347+Pablito1220@users.noreply.github.com>
2019-12-17 15:14:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-document.addEventListener("click", onClick);
 let container = document.getElementById("container");
 let counter = 0;
-let widthImg = 609 * 0.6;
-let heightImg = 609 * 0.6;
+let widthImg = 400;
+let heightImg = 400;
 let arrayImgs = document.getElementsByTagName("img");
 let nbImages = 18;
-onClick();
+let imgs = [];
+
+function setup() {
+  for (let i = 1; i < 19; i++) {
+    let image = new Image();
+    image.src = "img/img" + i + ".jpg";
+    imgs.push(image);
+  }
+  document.addEventListener("click", onClick);
+  onClick();
+}
+
 function onClick() {
-  let index = counter % nbImages + 1;
-  let image = new Image();
-  image.src = "img/img" + index + ".png"
+  let index = counter % nbImages;
   let img = document.createElement("img");
   img.className = "image";
-  img.src = image.src;
-  let top = Math.random() * (window.innerHeight - heightImg * 0.8);
-  let left = Math.random() * (window.innerWidth - (widthImg / 1.8));
+  img.src = imgs[index].src;
+  let top = Math.random() * (window.innerHeight - heightImg * 0.7);
+  let left = Math.random() * (window.innerWidth - (widthImg / 1.5));
   let rotation = getRandomArbitrary(2, -1) * 40;
   img.style.setProperty("top", top + "px");
   img.style.setProperty("transform", "rotate(" + rotation + "deg)");
@@ -22,15 +30,17 @@ function onClick() {
   img.className = "image"
   if (arrayImgs.length < 4) {
     document.body.appendChild(img);
-    console.log(arrayImgs[0])
   } else {
-    console.log(arrayImgs[0])
     arrayImgs[0].remove;
     document.body.appendChild(img);
   }
   counter++;
 }
+
 function getRandomArbitrary(range, min) {
   let value = Math.random() * range + min;
   return value
 }
+window.addEventListener("DOMContentLoaded", (event) => {
+  setup();
+});
