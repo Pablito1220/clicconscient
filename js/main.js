6532230e86b2f276287bff5f63d7b3ac946b6294
Pablito1220 <31880347+Pablito1220@users.noreply.github.com>
2019-12-17 14:05:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,32 +5,21 @@ let widthImg = 609 * 0.6;
 let heightImg = 609 * 0.6;
 let arrayImgs = document.getElementsByTagName("img");
 let nbImages = 18;
-
-
 onClick();
-
-
-
-
 function onClick() {
-
   let index = counter % nbImages + 1;
   let image = new Image();
   image.src = "img/img" + index + ".png"
   let img = document.createElement("img");
   img.className = "image";
   img.src = image.src;
-  let top = Math.random() * (window.innerHeight - heightImg * 0.7);
-  let left = Math.random() * (window.innerWidth - (widthImg / 1.5));
-  // let left = "200 px";
-  // let top = "200 px";
+  let top = Math.random() * (window.innerHeight - heightImg * 0.8);
+  let left = Math.random() * (window.innerWidth - (widthImg / 1.8));
   let rotation = getRandomArbitrary(2, -1) * 40;
   img.style.setProperty("top", top + "px");
   img.style.setProperty("transform", "rotate(" + rotation + "deg)");
   img.style.setProperty("left", left + "px");
   img.className = "image"
-
-
   if (arrayImgs.length < 4) {
     document.body.appendChild(img);
     console.log(arrayImgs[0])
@@ -38,22 +27,10 @@ function onClick() {
     console.log(arrayImgs[0])
     arrayImgs[0].remove;
     document.body.appendChild(img);
-    // document.body.replaceChild(img, arrayImgs);
   }
-  // if (counter < 2) {
-  //   document.body.appendChild(img);
-  // } else {
-  //   let documentLength = document.body.children.length
-  //   console.log(arrayImgs)
-  //   // document.body.children[documentLength].remove();
-  // }
   counter++;
-  console.log("top: " + top);
-  console.log("left: " + left)
 }
-
 function getRandomArbitrary(range, min) {
   let value = Math.random() * range + min;
-  // console.log(value)
   return value
 }
